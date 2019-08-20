95914fe5ff14780d57c269a0c4d405d03b543f35
Steve Halladay <shalladay@datastax.com>
2019-08-20 16:58:37 +0000

    Set IP to localhost

--- ip_address.js
+++ ip_address.js
@@ -3,7 +3,7 @@ const cassandra = require('cassandra-driver');
 
 function init_connection(){
     var connection = {}
-    connection.ip_address = '52.53.148.124';
+    connection.ip_address = '127.0.0.1';
     connection.dc = 'DC1'
     connection.path_to_creds=''
     connection.client = new cassandra.Client({ 
