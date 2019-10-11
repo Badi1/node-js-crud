a077f25c1233218159b5abf8a3b21a93c3767e0b
mando222 <eric.zietlow@datastax.com>
2019-10-11 11:24:35 -0400

    added apollo support

--- ip_address.js
+++ ip_address.js
@@ -1,15 +1,10 @@
 const cassandra = require('cassandra-driver');
 
-
 function init_connection(){
     var connection = {}
-    connection.ip_address = '127.0.0.1';
-    connection.dc = 'dc1'
-
-    connection.path_to_creds=''
     connection.client = new cassandra.Client({ 
-        contactPoints: [connection.ip_address],
-        localDataCenter: connection.dc 
+        cloud: { secureConnectBundle: 'secure-connect-NAME.zip' },
+        credentials: { username: 'USERNAME', password: 'PASSWORD' } 
     });
     return connection
 }
